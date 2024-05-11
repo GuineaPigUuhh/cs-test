ef56847178e2dfadd2afccc6a55f1652d1319d59
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-05-11 19:06:35 -0300

    testssss

---
+++ Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿
+public class Window
+{
+    private string name = "Test!";
+    private int width = 1280;
+    private int height = 720;
+
+    public Window(string name, int width, int height)
+    {   
+        this.name = name;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void printParams()
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(width);
+        Console.WriteLine(height);
+    }
+}
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Window windowTest = new Window("Cool Test!", 500, 200);
+        windowTest.printParams();
+    }
+}
---
+++ For/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program 
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"My first program! ({i})");
+        }
+
+        // * Separator
+        Console.WriteLine("---------------");
+
+        string[] collection = [
+            "Hello, World!",
+            "My first program!",
+            "Test!",
+            "Other test!",
+            "test?"
+        ];
+        foreach (var item in collection)
+        {
+            Console.WriteLine($"{item}");
+        }
+    }
+}
